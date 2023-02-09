a0bd694197fff0f4dd2f527141bc0186a0db6d37
Maldanar201 <manuel.aldana@unah.hn>
2023-02-09 14:32:53 -0600

    ejercicio 8
    ejercicio 8 funciones recursivas

--- 1erParcial/1erParcial/ejercicio8.cs
+++ 1erParcial/1erParcial/ejercicio8.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace _1erParcial
 {
-    public partial class ejercicio8 : Form
+    public partial class Ejercicio8 : Form
     {
-        public ejercicio8()
+        public Ejercicio8()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecorrerUnoEnUno(1, 100);
+        }
+
+        private void RecorrerUnoEnUno(int valorInicial, int valorFinal) 
+        {
+            listBox1.Items.Add(valorInicial);
+
+            if(valorInicial < valorFinal) 
+            {
+                RecorrerUnoEnUno(valorInicial + 1, valorFinal);
+            }
         }
     }
 }
